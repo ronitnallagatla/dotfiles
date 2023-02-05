a8ba0bbf2996ef5c80a08707f72de7d81bc64665
Ronit Nallagatla <ronit.nallagatla@gmail.com>
2023-02-04 20:19:35 -0800

    updates

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -14,6 +14,9 @@ alias cp='cp -i'
 alias mv='mv -i'
 alias rm='rm -i'
 
+# Clear
+alias c='clear'
+
 # SSH
 alias eceprog='kitty +kitten ssh rnallaga@eceprog.ecn.purdue.edu'
-alias mg='kitty +kitten ssh mg125@ececomp.ecn.purdue.edu'
+alias mg='kitty +kitten ssh mg252@ececomp.ecn.purdue.edu'
