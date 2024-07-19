ca779694eec1e22daadba00a035a64c566521c1a
Ronit Nallagatla <ronit.nallagatla@gmail.com>
2024-07-19 18:41:42 -0400

    updates

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -19,7 +19,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.lang.clangd" },
     { import = "lazyvim.plugins.extras.lang.python" },
-    { import = "lazyvim.plugins.extras.lang.python-semshi" },
+    -- { import = "lazyvim.plugins.extras.lang.python-semshi" },
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.test.core" },
     -- { import = "lazyvim.plugins.extras.ui.edgy" },
--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -15,22 +15,62 @@ return {
     },
   },
 
-  -- which key extensions
-  -- {
-  --   "folke/which-key.nvim",
-  --   opts = function()
-  --     require("which-key").register({
-  --       ["<leader>d"] = { name = "+debug", mode = { "n", "v" } },
-  --     })
-  --   end,
-  -- },
-  --
+  -- customize telescope
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = {
+      defaults = {
+        -- prompt_prefix = " ",
+        -- selection_caret = " ",
+        layout_strategy = "vertical",
+        layout_config = {
+          vertical = {
+            preview_cutoff = 0.2,
+            preview_height = 0.4,
+          },
+          height = 0.9,
+          width = 0.9,
+        },
+        mappings = {
+          i = {
+            ["<C-j>"] = function(...)
+              return require("telescope.actions").move_selection_next(...)
+            end,
+            ["<C-k>"] = function(...)
+              return require("telescope.actions").move_selection_previous(...)
+            end,
+            ["<C-p>"] = function(...)
+              return require("telescope.actions.layout").toggle_preview(...)
+            end,
+          },
+          n = {
+            ["j"] = function(...)
+              return require("telescope.actions").move_selection_next(...)
+            end,
+            ["k"] = function(...)
+              return require("telescope.actions").move_selection_previous(...)
+            end,
+            ["gg"] = function(...)
+              return require("telescope.actions").move_to_top(...)
+            end,
+            ["G"] = function(...)
+              return require("telescope.actions").move_to_bottom(...)
+            end,
+            ["<C-p>"] = function(...)
+              return require("telescope.actions.layout").toggle_preview(...)
+            end,
+          },
+        },
+      },
+    },
+  },
+
   -- Copilot which key extension
   {
     "folke/which-key.nvim",
     opts = {
-      defaults = {
-        ["<leader>a"] = { name = "+ai" },
+      spec = {
+        { "<leader>a", group = "ai" },
       },
     },
   },
